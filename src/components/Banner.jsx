6ef19c4c78d8862f2f7b50fdd86c2f2f92a61908
schandalette <schanda.lynn@gmail.com>
2024-08-31 18:18:23 -0500

    GA #6.1

---
+++ src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import styling from "./styling.css";
+
+function Banner() {
+    return (
+        <>
+            <header>Orbit Report</header>
+            <p>Click on the buttons to see the satellites in that orbit type</p>
+        </>
+    );
+}
+export default Banner;
--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,22 @@
+import styling from "./styling.css";
+import satData from "./satData";
+
 const Buttons = ({ filterByType, setSat, displaySats }) => {
   return (
-    <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
-    </div>
-  );
-};
+    <div className="flex-container">
+      {/* Filter buttons for individual sat types */}
+      {displaySats.map((sat, id) => {
+        return (
+          <button onClick={() => filterByType(sat)} key={id}>
+            {sat} Orbit
+          </button>
+        );
+
+      })}
+      {/* Button to display all orbits */}
+      < button onClick={() => setSat(satData)}> All Orbits</button >
+    </div >
+  )
+}
 
 export default Buttons;
