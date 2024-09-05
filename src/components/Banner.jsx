d8c865dfbe17da0d3cdfb23e6bb7b10f191bd74b
schandalette <schanda.lynn@gmail.com>
2024-09-04 22:52:30 -0500

    GA6 v2

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,10 +2,11 @@ import styling from "./styling.css";
 
 function Banner() {
     return (
-        <>
-            <header>Orbit Report</header>
+
+        <header>
+            <h1>Orbit Report</h1>
             <p>Click on the buttons to see the satellites in that orbit type</p>
-        </>
+        </header>
     );
 }
 export default Banner;
